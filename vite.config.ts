762c4640400f122d9daf0c13a8fa96f9da859c07
Safi649 <muhammadabbassafi332@gmail.com>
2025-09-17 01:14:28 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: '.', // root of project
   plugins: [react()],
-  root: '.',               // ensure root is client folder
-  publicDir: 'public',     // where your _redirects lives
   build: {
-    outDir: 'dist/public', // Netlify publish folder
+    outDir: 'dist/public', // output directory for Netlify
     emptyOutDir: true,
-  }
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // entry file
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
